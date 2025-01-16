088e8a84a1fb7416b45e3118a66437c91554d503
danda <ryousuke19990623@gmail.com>
2025-01-16 21:54:37 +0900

    学習記録フォーム表示・学習記録登録 作成

--- src/main/java/com/example/lrs/domain/learningRecord/LearningRecordService.java
+++ src/main/java/com/example/lrs/domain/learningRecord/LearningRecordService.java
@@ -8,10 +8,16 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class LearningRecordService {
-
+    //DI
     private final LearningRecordRepository learningRecordRepository;
 
-    public List<LearningRecordEntity> showList(){
+    //学習記録リスト表示
+    public List<LearningRecordEntity> showList() {
         return learningRecordRepository.showList();
+    }
+
+    //学習記録登録
+    public void createList(LearningRecordForm learningRecordform){
+        learningRecordRepository.createList(learningRecordform);
     }
 }
--- src/main/java/com/example/lrs/web/LearningRecordController.java
+++ src/main/java/com/example/lrs/web/LearningRecordController.java
@@ -1,10 +1,13 @@
 package com.example.lrs.web;
 
+import com.example.lrs.domain.learningRecord.LearningRecordForm;
 import com.example.lrs.domain.learningRecord.LearningRecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -20,5 +23,18 @@ public class LearningRecordController {
         model.addAttribute("learningList",learningRecordService.showList());
         return "learningRecord/list";
 
-    };
+    }
+
+    //学習記録フォーム表示
+    @GetMapping("/form")
+    public String showForm(@ModelAttribute LearningRecordForm learningRecordForm){
+        return "learningRecord/form";
+    }
+
+    //学習記録登録
+    @PostMapping("/create")
+    public String createForm(@ModelAttribute LearningRecordForm learningRecordform){
+        learningRecordService.createList(learningRecordform);
+        return "redirect:learningRecord/list";
+    }
 }
